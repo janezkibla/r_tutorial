f3e0857cd4c55a0b81e52dd4b279a36195fd7ac5
Johan Luštrik <lustrikjohan@gmail.com>
2022-05-09 18:05:52 +0200

    Variables, Input and output

--- R_tutorials.r
+++ R_tutorials.r
@@ -129,60 +129,57 @@ print (1:15)
 
 # %*% operator naredi vrste 
 
-mat <- matrix (1:4, nrow = 1, ncol = 4)
-print("Matrix elements using : ")
+mat = matrix(c(1,2,3,4,5,6),nrow, ncol=3)
 print(mat)
-
-product = mat %*% t(mat)
-
-# if 
-
-a <- 5
-if( a > 0)
-  print("Positive Number")
-
-# else
-
-x <- 1
-
-if(x > 10)
-
-  print(paste(x, "is greater than 10")) 
+print( t(mat))
+pro = mat %*% t(mat)
+print(pro)
 
 # else -apart of code
 
+x <- 5
+
+if(x > 10)
+{ 
+  print(paste(x, "is greater than 10")) 
+}
+else
+{ 
   print(paste(x, "is less than 10")) 
+}
 
 # while
-  
+
 val = 1
 while (val <= 5)
+{  
   print(val)
   val = val +1
-  
+}
 # repeat
 
 val = 1
 repeat
   print(val)
-  val = val + 1
-  
-  if (val > 5)
-    break
+val = val + 1
+
+if (val > 5)
+  break
 
 # for  konstrucia loopa
-  
-for (val in 1:5)
-  print(val)
 
+for (val in 1:5)
+{  
+  print(val)
+}
 # function 
 
-evenOdd = function(x)
+evenOdd = function(x){
   if(x %% 2 == 0)
     return("even")
 else
-    return("odd")
-
+  return("odd")
+}
 print(evenOdd(4))
 print(evenOdd(3))
 
@@ -191,22 +188,23 @@ print(evenOdd(3))
 val <- 6 : 11
 
 for (i in val)
-  
+{  
   if (i == 8)
-    
+  {  
     next
-
-  print(i)
-
+  }  
+print(i)
+}
 # break - breaks the loop
 
 a<-2
 while (a < 10)
-  
+{  
   print(a)
-  if(a == 5)
-    break
-  a = a +1
+if(a == 5)
+  break
+}
+a = a +1
 
 # TRUE/FALSE  peikazuje Boolean true in Boolean false
 
@@ -302,3 +300,524 @@ print(as.complex(3L))
 print(as.logical(10.5))
 print(as.character(1+2i))
 print(as.numeric("12-04-2020"))
+
+
+# Variables
+# ----------
+
+# Creating variables in R
+
+var1 = "hello"
+print(var1)
+
+var2 ="how"
+print(var2)
+
+var3 = "you"
+print(var3)
+
+# class()function
+
+var1 = "hello"
+print(class(var1))
+
+#is ()function
+
+var1 = "hello"
+var2 <- "hello"
+"hello" -> var3
+
+print(ls())
+
+# rm()function
+
+var1 = "hello"
+var2 <- "hello"
+"hello" -> var3
+
+rm(var3)
+print(var)
+
+# Global variable
+
+global = 5
+
+display = function(){
+  print(global)
+}
+display ()
+
+global = 10
+display()
+
+# Declaring local variables
+
+func = function(){
+  
+  age = 18
+print(age)
+}
+cat("Age is:\n")
+func()
+
+# Scope of variable in r
+
+var1 = c(0, 1, 2, 3)
+print(var1)
+
+var2 <- c("Python", "R")
+print(var2)
+
+a = c(1,2, 3, 4)
+print(a)
+b = c("Debi", "Sadeep", "Suham", "Shiba")
+print(b)
+
+c = list(a, b)
+print(c)
+
+# Naming conventio for variables
+
+b2 = 7 #correct naming
+
+Amiya_Profession = "Student" #correct naming
+
+Amiya.Profession = "Studen" #correct naimg
+
+2b = 7 #wrong naming
+
+Amiya@Profession = "Student" #wrong naming
+
+# global veriable 
+
+global = 5
+display = funcion(){
+print(global)
+}  
+display()
+
+global = 10
+display()
+
+# Local variables
+
+func = funcion(){
+age = 18
+}
+print(age)
+
+# Accessing global variables
+
+f = function(){
+  a <-1
+}  
+f()
+
+print(a)
+
+# Dynamic scoping in R programing
+
+make.power <- function(n){
+  pow <- function(x){
+    x = x^n
+  }
+pow
+}
+
+cube <- make.power(3)
+square <- make.power(2)
+print(cube(3))
+print(square(3))
+
+# The power function takes the argument x and raised the power n. 
+# So that makes the function pow return inside the function value.
+# n is a free variable and is defines in the pow function.
+
+ls(environment(cube))
+
+get ("n", environment(cube))
+
+ls(environment(square))
+
+get("n", environment(square))
+
+
+# Lexical scoping in R proraming
+
+f <- function(x, y)
+{  
+  x * y * z
+}
+# Name maskin
+
+c <- 10 
+f <- function(a, b)
+{  
+  a + b + c
+}
+f(8, 5)
+
+# ce veriable ni definiran v funkciji
+
+a <- 10 
+b <- funcion()
+{
+c <- 11
+c(a, c)
+}
+b()    
+
+# ko 1 funkcija je definiran v drugi funkciji
+
+a <- 10 
+g <- function(){
+  b <- 20 
+  h <- function(){
+    c <- 30
+    c(a, b, c)
+  }
+h()
+}
+g()    
+
+# ko druga cunkcija naredi funkcijo
+
+a <- function(z){
+  b <- 10
+  function(){
+    z + 4 * b
+  }
+}
+x <- a(10)
+x()
+
+# functions vs variables
+
+a <- function(x) 10 * x 
+b <- function(){
+  a <- function(x) x + 10
+  a(12)
+}
+b()
+
+# svezi zacetek
+
+a <- function(){
+  if(!exists("z"))
+  {
+    z <- 10
+  }
+  else
+  {
+    z <- z+10
+  }
+  z
+}
+a()
+
+# dynamic lockup
+
+g <- function() x^3
+x <- 10 
+g()
+
+# findGlobals
+
+aGlobal <- rnorm(10)
+bGlobal <- rnorm(10)
+
+f <- function()
+{  
+  a <- aGlobal
+  b <- bGlobal
+  plot(b ~ a)
+}
+codetools::findGlobals(f)
+
+# Lexical scoping vs Dynamic scoping in R programing
+
+a <- 1 
+
+b <- function() a
+c <- function(){
+  a <- 2
+  b()
+}
+c()
+
+# dynmic scoping
+
+a <-1
+
+b <- function()a
+c <- function(){
+  a <- 2
+  b()
+}
+c()    
+
+#scope in R launguage
+
+a <- 10
+
+f <- function(x) {
+  a <- 2
+  a ^ 2 + g(x)
+}
+g <- function(x){ 
+  x * a
+}
+f(3)
+
+# Input and output
+# --------------------
+
+var = readline();
+
+var = as.integer(var);
+
+print(var)
+
+# lahkonapisemo messages v console
+
+var = realine(prompt = "Enter any nuber : ");
+var = as.integer(var);
+print(var)
+
+# taking multiple unputs in R
+
+{
+  var1 = readline("Enter 1st number : ");
+  var2 = readline("Enter 2nd number : ");
+  var3 = readline("Enter 3rd number : ");
+  var4 = readline("Enter 4th number : ");
+}
+
+
+var1 = as.integer(var1);
+var2 = as.integer(var2);
+var3 = as.integer(var3);
+var4 = as.integer(var4);
+
+print(var1 + var2 +var3 + var4)
+
+# taking string charecter input in R
+
+var1 = readline(prompt = "Enter your name : ");
+
+var2 = readline(prompt = "Enter any charecter");
+
+var2= as.charecter(var2)
+
+print(var1)
+print(var2)
+
+# using scan()
+
+x = scan()
+
+print(x)
+
+# double string charecters
+
+d = scan(what = double())
+
+s = scan(what = " ")
+
+c = scan(what = charecter())
+
+print(d)
+print(s)
+peint(c)
+
+# read file data using scan()method
+
+s = scan("filsString-txt", what = " ")
+
+d = scan("fileDouble-txt", what = double())
+
+c = scan("fileChar.txt", what = charecter())
+
+print(s)
+print(d)
+print(c)
+
+# Printing out of an R program
+
+x <- "Johan"
+x
+
+# print output using print() function
+
+print("jhn")
+
+x <- "johan"
+print(x)
+
+# print output using paste() function inside print() function
+
+x <- "Joe mama"
+
+print(paste(x, "is best (paste inside print())"))
+print(paste(x, "is best (paste0 inside print())"))
+
+# print outout using sprintf()functions
+
+x = "Johan"
+x1 = 255
+x2 = 23.14
+
+sprintf("%s is best", x)
+
+sprintf("%d is intiger", x1)
+
+sprintf("%f is float", x2)
+
+# print output using cat()function
+
+x = "joe mama"
+cat(x, "is best\n")
+cat("This is r launguage")
+
+# print output using message()function 
+
+x = "Joe mama"
+
+message(x, "is best")
+message("This is r launguage")
+
+# write outout to a file 
+
+x = "joe mama"
+
+write.table(x, file = "my_data1.txt")
+
+write.table("jhn is best", file = "my_data2.txt")
+
+# print()functions in r launguage is used to print out argument to the screen
+
+x <- cars[1:5 ]
+
+print(x)
+
+# example 2
+
+x <- 15 / 7
+
+print(x, digits = 2)
+print(x, digits = 3)
+print(x, digits = 4)
+
+# example 3
+
+x <- matrix(c(2, NA , 5 ,NA, 8, NA, 22, 67, 43),
+            nrow = 3, byrow = TRUE)
+print(x, na.print = "")
+
+# Decision Making 
+# ---------------------------
+
+# is statement
+
+a <- 76
+b <- 67
+
+if(a > b)
+{  
+  c <- a - b
+  print("condition a > b is TRUE")
+  print(paste("Difference between a, b is : ", c))
+}
+
+if(a < b)
+{
+  c <- a - b
+  print("condition a < b is TRUE")
+  print(paste("Difference between a, b is : ",c))
+}
+# if-else statement
+
+a <- 67
+b <- 76
+
+if(a > b)
+{  
+  c <- a - b
+  print("condition a > b is TRUE")
+  print(paste("Difference between a, b is : ", c))
+}else
+{  
+  c <- a - b
+  print("condition a > b is FALSE")
+  print(paste("Difference between a, b is : ", c))
+}
+
+# if-else-if ladder
+
+a <- 67
+b <- 76
+c <- 99
+
+if(a > b && b > c)
+{
+  print("condition a > b > c is TRUE")
+}else if(a < b && b > c)
+{  
+  print("condition a < b > c is TRUE")
+}else if(a < b && b < c)
+{  
+  print("condition a < b < c is TRUE")
+}
+# nested if-else statement
+
+a <- 10
+b <- 11
+
+if(a == 10)
+{
+  if(b == 10)
+  {
+    print("a:10 b:10")
+  }else
+  {
+    print("a:10 b:11")
+  }
+}else
+{
+  if(a == 11)
+  {
+    print("a:11 b:10")
+  }else
+  {
+    print("a:11 b:11")
+  }  
+}  
+
+
+# switch statement
+
+x <- switch(
+  2,           # Expression
+  "Geeks1",    # case 1
+  "for",       # case 2
+  "Geeks2"     # case 3
+)
+print(x)
+
+# Expression in terms of the string value
+y <- switch(
+  "GfG3",             
+  "GfG0"="Geeks1",     
+  "GfG1"="for",        
+  "GfG3"="Geeks2"      
+)
+print(y)
+
+z <- switch(
+  "GfG",               
+  "GfG0"="Geeks1",     
+  "GfG1"="for",        
+  "GfG3"="Geeks2"   
+)
+print(z)
+
